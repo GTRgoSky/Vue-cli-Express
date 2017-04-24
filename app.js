ce5c26c365d6dc91e783ccc7892f452564f4c7ce
范翔 <fx36023@ly.com>
2017-04-24 16:36:01 +0800

    放入到Express的App.js中

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+'use strict';
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
+// var bodyParser = require('body-parser');
+// 引入文件模块
+const fs = require('fs');
+// 引入处理post数据的模块
+const bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+// 引入编写好的api
+const api = require('./server/api'); 
 var app = express();
 
 // view engine setup
@@ -19,11 +24,17 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// app.use(cookieParser());
+app.use(express.static(path.resolve(__dirname, './my-vue/dist')));
+app.use(api);
+// app.use('/', routes);
+// app.use('/users', users);
+// app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', function(req, res) {
+    const html = fs.readFileSync(path.resolve(__dirname, './my-vue/dist/index.html'), 'utf-8')
+    res.send(html)
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
